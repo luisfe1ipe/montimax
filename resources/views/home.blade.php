72ecc93fe616a62618e2c614593031bd32be976e
Luis Felipe <108816416+luisfe1ipe@users.noreply.github.com>
2022-11-01 14:34:39 -0300

    iniciando

---
+++ resources/views/home.blade.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio - MONTIMAX</title>
+
+
+    <link rel="stylesheet" href="/css/app.css">
+
+
+
+    <!-- FONTES -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700;800&display=swap" rel="stylesheet">
+</head>
+<body>
+
+
+    <nav>
+        <div class="navbar">
+
+            <a href="index.html"><img src="/assets/img/logo.png" alt=""></a>
+            <div class="mobile-menu">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+            <ul class="nav-list">
+                <li><a href="index.html" id="navbar-ativo">INICIO</a></li>
+                <li><a href="projetos.html">PROJETOS</a></li>
+                <li><a href="contato.html">CONTATO</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <section class="img-painel">
+        <div class="container-painel">
+            <div class="conteudo-painel">
+                <h1>Precisa de algo ? Entre em contato</h1>
+                <form action="/contato.html">
+                    <button type="submit">Contato</button>
+                </form>
+            </div>
+        </div>
+    </section>
+
+    <div class="container-section">
+        <div class="container-about">
+            <h1 class="titulo"><div class="destaque"></div>Sobre Nós</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aut est illo quidem culpa dolor dolores nam odio porro commodi recusandae dignissimos quibusdam sequi id explicabo, adipisci quam error aliquid!
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio sed sequi quibusdam id voluptas ut, harum aliquid perspiciatis officia itaque tenetur, quo animi quos amet? Vitae error reiciendis temporibus aspernatur.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam libero molestias veniam modi totam, voluptatum quo, soluta sit eveniet, dolores aspernatur consequuntur aut? Quam, repudiandae incidunt assumenda commodi ipsa at!
+            </p>
+        </div>
+        <div class="container-projetos">
+            <h1 class="titulo"><div class="destaque"></div>Projetos</h1>
+            <div class="list-projetos">
+
+                <a href="/visualizar-projeto.html" class="link-projeto">
+                    <div class="projeto">
+                        <img src="/assets/img/projeto.png" alt="">
+                        <div class="container-desc">
+                            <div class="sub">
+                                <div class="data">
+                                    <img src="/assets/svg/calendario.svg" alt="">
+                                    <p>2022</p>
+                                </div>
+                                <div class="categoria">
+                                    <p>Quimica e Petroquimicas</p>
+                                </div>
+                            </div>
+                            <div class="descricao">
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aliquid nam officia amet. Modi nemo
+                                    quis ipsum facere ipsa et possimus quod quas illum dolorem, natus provident similique saepe laudantium?
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi numquam quo quod eligendi quisquam
+                                    quibusdam at fugit ullam doloribus deserunt similique repellendus accusantium voluptates, distinctio
+                                    ducimus iure saepe libero reprehenderit!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="" class="link-projeto">
+                    <div class="projeto">
+                        <img src="/assets/img/projeto.png" alt="">
+                        <div class="container-desc">
+                            <div class="sub">
+                                <div class="data">
+                                    <img src="/assets/svg/calendario.svg" alt="">
+                                    <p>2022</p>
+                                </div>
+                                <div class="categoria">
+                                    <p>Quimica e Petroquimicas</p>
+                                </div>
+                            </div>
+                            <div class="descricao">
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aliquid nam officia amet. Modi nemo
+                                    quis ipsum facere ipsa et possimus quod quas illum dolorem, natus provident similique saepe laudantium?
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi numquam quo quod eligendi quisquam
+                                    quibusdam at fugit ullam doloribus deserunt similique repellendus accusantium voluptates, distinctio
+                                    ducimus iure saepe libero reprehenderit!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="" class="link-projeto">
+                    <div class="projeto">
+                        <img src="/assets/img/projeto.png" alt="">
+                        <div class="container-desc">
+                            <div class="sub">
+                                <div class="data">
+                                    <img src="/assets/svg/calendario.svg" alt="">
+                                    <p>2022</p>
+                                </div>
+                                <div class="categoria">
+                                    <p>Quimica e Petroquimicas</p>
+                                </div>
+                            </div>
+                            <div class="descricao">
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aliquid nam officia amet. Modi nemo
+                                    quis ipsum facere ipsa et possimus quod quas illum dolorem, natus provident similique saepe laudantium?
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi numquam quo quod eligendi quisquam
+                                    quibusdam at fugit ullam doloribus deserunt similique repellendus accusantium voluptates, distinctio
+                                    ducimus iure saepe libero reprehenderit!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+
+            </div>
+            <div class="view-all">
+                <form action="/projetos.html">
+                    <button class="all-button">Ver todos projetos</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <div class="container-footer">
+           <div class="conteudo-footer">
+                <p>(18) 99999-9999</p>
+                <p>exemplo@gmail.com</p>
+                <p>Rua xxxxxxx, XX - Tarumã - SP</p>
+           </div>
+        </div>
+    </footer>
+
+
+
+
+
+
+<script src="/assets/js/mobile-navbar.js"></script>
+
+
+</body>
+</html>
